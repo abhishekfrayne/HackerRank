3b0f03974ae93d62a7e8f816d901316a99694154
JaDe <fatal36z@gmail.com>
2016-02-22 14:36:26 +0800

    Added Contest "30 days of code"

---
+++ 30 days of code/Day 0 - Print Hello World/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+       public static void main(String[] args) {
+        
+          // Write your print statements here!! 
+          // Template: System.out.println("what you want to print");
+            System.out.println("Hello World.\nWelcome to 30 Days of Code.");
+       }
+   }
